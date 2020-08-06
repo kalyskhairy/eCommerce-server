46b0d8c9883483979053dba1de446c07619749f6
kalys <61675998+kalyskhairy@users.noreply.github.com>
2020-08-06 17:33:47 +0700

    commit v2.2

---
+++ seeders/20200806102409-add-product.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Products = require('../product.json').map(product => {
+  product.createdAt = new Date()
+  product.updatedAt = new Date()
+  return product
+})
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Products', Products, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Products', null, {})
+  }
+};
